ef6616be99c63bcd5bb53374e616cb3caa9147d5
damithaudesh662 <damithavidanapathirana2000@gmail.com>
2023-05-09 15:02:06 +0530

    first commit

---
+++ Lab8/01FIRSTSub.asm
@@ -0,0 +1,5 @@
+CLO		; Close unwanted windows.
+MOV AL,2	; Copy a 2 into the AL register.
+MOV BL,2	; Copy a 2 into the BL register.
+SUB AL,BL	; Add AL to BL. Answer goes into AL.
+END		; Program ends
